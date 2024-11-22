da600d4cb22081c000a38823017c380f83629502
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-22 12:02:29 +0000

    added try&except for getting wpm functionality

--- tutorial.py
+++ tutorial.py
@@ -10,45 +10,52 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 def display_text(stdscr, target, current, wpm=0):
-    stdscr.addstr(target)
-    stdscr.addstr(1, 0, f"WPM: {wpm}")
+    # Display target text
+    stdscr.addstr(0, 0, target)
 
+    # Display current text with color highlighting
     for i, char in enumerate(current):
         correct_char = target[i]
-        color = curses.color_pair(1)
+        color = curses.color_pair(1)  # Green for correct
         if char != correct_char:
-            color = curses.color_pair(2)
+            color = curses.color_pair(2)  # Red for incorrect
+        stdscr.addstr(0, i, char, color)
 
-        stdscr.addstr(0, i, char, curses.color_pair(1))
+    # Display WPM
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
 def wpm_test(stdscr):
     target_text = "Hello world"
     current_text = []
     wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear()
         display_text(stdscr, target_text, current_text, wpm)
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
-        if ord(key) == 27:
+        if ord(key) == 27:  # Escape key to exit
             break
-        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):  # Handle backspace
             if len(current_text) > 0:
                 current_text.pop()
-        elif len(current_text) < len(target_text):           
+        elif len(current_text) < len(target_text):  # Add valid characters
             current_text.append(key)
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Correct character
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)    # Incorrect character
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Default
 
     start_screen(stdscr)
     wpm_test(stdscr)
